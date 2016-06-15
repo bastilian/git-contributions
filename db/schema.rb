4bf05d9dd012059823a20bf2de4648e8b07d211a
Sebastian Gräßl <sebastian@validcode.me>
2016-06-15 13:32:09 +0200

    Add Author model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615103536) do
+ActiveRecord::Schema.define(version: 20160615112519) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "repositories", force: :cascade do |t|
     t.string   "name"
