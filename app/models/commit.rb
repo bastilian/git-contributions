e033fdb3c20b113ca9f94e3b4338a31abee5abc2
Sebastian Gräßl <sebastian@validcode.me>
2016-06-15 14:31:21 +0200

    Validate presence of Commit author, repository and commited_at

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,4 +1,8 @@
 class Commit < ActiveRecord::Base
   belongs_to :author
   belongs_to :repository
+
+  validates_presence_of :author
+  validates_presence_of :repository
+  validates_presence_of :committed_at
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
+  expect_it { to validate_presence_of :author }
+  expect_it { to validate_presence_of :repository }
+  expect_it { to validate_presence_of :committed_at }
 end
