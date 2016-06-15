43ecd6540d40591031ffbac1fae2ca1e05c93967
Sebastian Gräßl <sebastian@validcode.me>
2016-06-15 13:44:34 +0200

    Associate Author with emails

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   expect_it { to validate_presence_of :name }
+  expect_it { to have_many :emails }
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Email, type: :model do
+  expect_it { to belong_to :author }
 end
