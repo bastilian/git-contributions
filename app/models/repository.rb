007774b0c962df3a13418e46f79710a31e2be5b6
Sebastian Gräßl <sebastian@validcode.me>
2016-06-15 13:23:37 +0200

    Implement cloning repository into tmp folder

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,7 @@
 class Repository < ActiveRecord::Base
   validates_presence_of :url
+
+  def clone
+    system("git clone #{url} ./tmp/repositories/#{id}")
+  end
 end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Repository, type: :model do
   subject { FactoryGirl.create(:repository) }
 
   expect_it { to validate_presence_of :url }
+
+  describe "#clone" do
+    it "clones the repository via system" do
+      expect(subject).to receive(:system).with("git clone #{subject.url} ./tmp/repositories/#{subject.id}")
+      subject.clone
+    end
+  end
 end
