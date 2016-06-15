a0e35bea68d1303b25496127612757ad9c203832
Sebastian Gräßl <sebastian@validcode.me>
2016-06-15 14:01:07 +0200

    Validate the presence of an address in Email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,5 @@
 class Email < ActiveRecord::Base
   belongs_to :author
+
+  validates_presence_of :address
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Email, type: :model do
   expect_it { to belong_to :author }
+  expect_it { to validate_presence_of :address }
 end
