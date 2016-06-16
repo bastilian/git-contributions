86e2abead7713b2809d23ddf7b2181be346547d3
Sebastian Gräßl <sebastian@validcode.me>
2016-06-16 13:44:27 +0200

    Document Repository model and methods

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,18 +1,22 @@
+# Repository Model that holds all the commits
 class Repository < ActiveRecord::Base
   validates_presence_of :url
 
   has_many :commits
 
+  # Clones a repository from a git url/path
   def clone
     system("git clone #{url} /tmp/repositories/#{id}")
   end
 
+  # Imports commits from log entries
   def import
     log.each do |commit|
       commits.create(commit)
     end
   end
 
+  # Returns an array of commit log entries from a raw git log
   def log
     raw_log.each_line.map do |raw_commit|
       commit = raw_commit.split('|')
@@ -28,6 +32,7 @@ class Repository < ActiveRecord::Base
     end
   end
 
+  # Returns the git commit log as a String
   def raw_log
     `cd /tmp/repositories/#{id} && git log --pretty="%cd|%aN|%ae|%s"`
   end
