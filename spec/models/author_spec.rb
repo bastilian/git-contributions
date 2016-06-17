60cf124d3097f77a669047a5a618f21027cd7ee9
Sebastian Gräßl <sebastian@validcode.me>
2016-06-17 13:38:33 +0200

    Add specs for Author.with_email

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Author, type: :model do
   expect_it { to have_many :emails }
   expect_it { to have_many :commits }
   expect_it { to have_many :repositories }
+
+  describe '.with_email' do
+    let(:author) { FactoryGirl.create(:email).author }
+
+    it 'returns an author having the matching email' do
+      expect(Author.with_email(author.emails.first.address).first).to eq(author)
+    end
+  end
 end
