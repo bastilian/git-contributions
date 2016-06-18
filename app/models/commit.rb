3d88188e37a3f2408c6afd2c61192f699503fb8f
Sebastian Gräßl <sebastian@validcode.me>
2016-06-18 08:35:02 +0200

    Validate presence and uniqunes of commit sha

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,9 +3,8 @@ class Commit < ActiveRecord::Base
   belongs_to :author
   belongs_to :repository
 
-  validates_presence_of :author
-  validates_presence_of :repository
-  validates_presence_of :committed_at
+  validates_presence_of :sha, :author, :repository, :committed_at
+  validates_uniqueness_of :sha
 
   accepts_nested_attributes_for :author
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
+  expect_it { to validate_presence_of :sha }
   expect_it { to validate_presence_of :author }
   expect_it { to validate_presence_of :repository }
   expect_it { to validate_presence_of :committed_at }
+  expect_it { to validate_uniqueness_of :sha }
 end
