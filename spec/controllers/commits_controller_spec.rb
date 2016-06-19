31da3b87bfdcf2a32c77ad1a54b685e39053f2a4
Sebastian Gräßl <sebastian@validcode.me>
2016-06-19 13:28:49 +0200

    Correct wording for CommitsController spec

--- spec/controllers/commits_controller_spec.rb
+++ spec/controllers/commits_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe CommitsController, type: :controller do
   end
 
   describe 'GET #index' do
-    it 'assigns all repositories as @repositories' do
+    it "assigns all commits as @commits and it's repository as @repository" do
       get :index, repository_id: repository.to_param
       expect(assigns(:repository)).to eq(repository)
       expect(assigns(:commits)).to eq(repository.commits.all)
@@ -19,7 +19,7 @@ RSpec.describe CommitsController, type: :controller do
   describe 'GET #show' do
     let(:last_commit) { repository.commits.last }
 
-    it 'assigns the requested repository as @repository' do
+    it "assigns the requested commit as @commit and it's repository as @repository" do
       get :show, repository_id: repository.to_param, id: last_commit.to_param
       expect(assigns(:repository)).to eq(repository)
       expect(assigns(:commit)).to eq(last_commit)
