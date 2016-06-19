97d5bf21a9c21fa5e5ff0c9ff7a17c22623872e6
Sebastian Gräßl <sebastian@validcode.me>
2016-06-19 13:37:13 +0200

    Remove set_commit as before_action and query in CommitsController#show

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,6 +1,5 @@
 class CommitsController < ApplicationController
-  before_action :set_commit, only: [:show]
-  prepend_before_action :set_repository
+  before_action :set_repository
 
   # GET /repositories/repository_id/commits
   # GET /repositories/repository_id/commits.json
@@ -11,15 +10,12 @@ class CommitsController < ApplicationController
   # GET /repositories/repository_id/commits/1
   # GET /repositories/repository_id/commits/1.json
   def show
+    @commit = @repository.commits.find(params[:id])
   end
 
   private
 
   def set_repository
     @repository = Repository.find(params[:repository_id])
-  end
-
-  def set_commit
-    @commit = @repository.commits.find(params[:id])
   end
 end
