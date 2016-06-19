da7e417633f9e69c447f7cf8cc8dec7f3ee68d12
Sebastian Gräßl <sebastian@validcode.me>
2016-06-19 09:30:31 +0200

    Refactor Repository#import to use Commit.create_from_git_commit

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -17,30 +17,10 @@ class Repository < ActiveRecord::Base
 
   # Imports commits from log entries
   def import
-    log.each do |commit|
-      next if Commit.where(sha: commit[:sha]).first
-      commit = commits.create(commit)
-      author = Author.with_email(commit.author.emails.first.address).first
-      commit.author = author if author
+    local.log.each do |commit|
+      next if Commit.where(sha: commit.sha).first
+      commit = commits.create_from_git_commit(commit)
       commit.save!
-    end
-  end
-
-  # Returns an array of commit log entries from a raw git log
-  def log
-    local.log.map do |commit|
-      {
-        sha: commit.sha,
-        committed_at: commit.date.to_datetime,
-        deletions: commit.diff_parent.stats[:deletions].to_i,
-        additions: commit.diff_parent.stats[:additions].to_i,
-        author_attributes: {
-          name: commit.author.name,
-          emails_attributes: [
-            address: commit.author.email
-          ]
-        }
-      }
     end
   end
 end
--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -22,30 +22,11 @@ RSpec.describe Repository, type: :model do
     end
   end
 
-  describe '#log' do
-    it 'returns an array' do
-      expect(subject.log.class).to eq(Array)
-    end
-
-    it 'contains a Hash for each commit' do
-      expect(subject.log.first.class).to eq(Hash)
-    end
-
-    it 'contains a committed_at date and author with email and name' do
-      expect(subject.log.first[:committed_at].class).to eq(DateTime)
-      expect(subject.log.first[:deletions].class).to eq(Fixnum)
-      expect(subject.log.first[:additions].class).to eq(Fixnum)
-      expect(subject.log.first[:author_attributes]).to_not be_nil
-      expect(subject.log.first[:author_attributes][:emails_attributes]).to_not be_nil
-      expect(subject.log.first[:author_attributes][:name]).to_not be_nil
-    end
-  end
-
   describe '#import' do
     it 'creates commits for each entry in #log' do
       expect do
         subject.import
-      end.to change(Commit, :count).by(subject.log.length)
+      end.to change(Commit, :count).by(subject.local.log.size)
     end
 
     it 'does not import commits twice' do
