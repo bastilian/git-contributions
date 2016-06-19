a9c85e651d8a0adefab6a74a01da25b16b08ff74
Sebastian Gräßl <sebastian@validcode.me>
2016-06-19 09:28:31 +0200

    Add Commit.create_from_git_commit and Commit.attributes_from_git_commit

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -8,4 +8,28 @@ class Commit < ActiveRecord::Base
   validates_uniqueness_of :sha
 
   accepts_nested_attributes_for :author
+
+  class << self
+    def attributes_from_git_commit(commit)
+      {
+        sha: commit.sha,
+        committed_at: commit.date.to_datetime,
+        deletions: commit.diff_parent.stats[:deletions].to_i,
+        additions: commit.diff_parent.stats[:additions].to_i,
+        author_attributes: {
+          name: commit.author.name,
+          emails_attributes: [
+            address: commit.author.email
+          ]
+        }
+      }
+    end
+
+    def create_from_git_commit(commit)
+      commit = create(attributes_from_git_commit(commit))
+      author = Author.with_email(commit.author.emails.first.address).first
+      commit.author = author if author
+      commit
+    end
+  end
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
+  let(:repository) { FactoryGirl.create(:repository) }
+  let(:log) { repository.local.log }
+
   expect_it { to validate_presence_of :sha }
   expect_it { to validate_presence_of :author }
   expect_it { to validate_presence_of :repository }
   expect_it { to validate_presence_of :committed_at }
   expect_it { to validate_uniqueness_of :sha }
+
+  describe '.attributes_from_git_commit' do
+    subject { Commit.attributes_from_git_commit(log.first) }
+
+    it 'contains a committed_at date and author with email and name' do
+      expect(subject[:committed_at].class).to eq(DateTime)
+      expect(subject[:deletions].class).to eq(Fixnum)
+      expect(subject[:additions].class).to eq(Fixnum)
+      expect(subject[:author_attributes]).to_not be_nil
+      expect(subject[:author_attributes][:emails_attributes]).to_not be_nil
+      expect(subject[:author_attributes][:name]).to_not be_nil
+    end
+  end
+
+  describe '.create_from_git_commit' do
+    let(:commit) { log.first }
+    it 'gets attributes via attributes_from_git_commit' do
+      expect(Commit).to receive(:attributes_from_git_commit).with(commit).and_call_original
+
+      Commit.create_from_git_commit(commit)
+    end
+  end
 end
